585f7dc5a37bacf20aa0ec50abf510073fbdc0af
vangarilokesh <98004966+vangarilokesh@users.noreply.github.com>
2023-02-22 12:03:56 +0530

    crud

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,11 @@ import "./App.css";
 import Login from "./Components/Login";
 import Register from "./Components/SignUp";
 import Home from "./Components/Home"
+import Create from "./Components/Create"
+import Read from "./Components/Read"
+import Update from "./Components/Update"
+import Delete from "./Components/Delete"
+import Search from "./Components/Search"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
@@ -13,6 +18,11 @@ function App() {
           <Route path="/home" element={<Home />}></Route>
           <Route path="/" element={<Login />}></Route>
           <Route path="/signup" element={<Register />}></Route>
+          <Route path="/create" element={<Create />}></Route>
+          <Route path="/read" element={<Read />}></Route>
+          <Route path="/update" element={<Update />}></Route>
+          <Route path="/delete" element={<Delete />}></Route>
+          <Route path="/search" element={<Search />}></Route>
         </Routes>
       </Router>
     </div>
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,11 +1,41 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useState } from "react-router-dom";
 
 function Home() {
-  const location = useLocation()
+  // const [student, setStudent] = useState("");
+
+  // async function submit(e) {
+  //   e.preventDefault();
+
+  //   try {
+  //     await axios
+  //       .get("http://127.0.0.1:8000/read")
+  //       .then((res) => {
+  //         console.log(res);
+  //         arr = res.data;
+  //         console.log(arr.data);
+  //       })
+  //       .catch((e) => {
+  //         alert("wrong details Read-24");
+  //         console.log(e);
+  //       });
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
+
   return (
     <div className="homepage">
-      <h1>Hello {location.state.id} and welcome to the home</h1>
+      <nav>
+        <ul>
+          <li><a href="/create">Create</a></li>
+          <li><a href="/read">Read</a></li>
+          <li><a href="/update">Update</a></li>
+          <li><a href="/delete">Delete</a></li>
+          <li><a href="/search">Search</a></li>
+        </ul>
+      </nav>
+
     </div>
   );
 }
